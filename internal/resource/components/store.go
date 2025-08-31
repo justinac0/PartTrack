e1c78f0bd2e947ae6ebd26dda236e015f2829c6d
justinac0 <justinac.1999@gmail.com>
2025-08-31 23:43:21 +1000

    modified:   internal/resource/components/store.go

--- internal/resource/components/store.go
+++ internal/resource/components/store.go
@@ -10,6 +10,12 @@ import (
 	"fmt"
 )
 
+type StoreError error
+
+var (
+	ErrPageOutOfBounds StoreError = errors.New("page out of bounds")
+)
+
 type ComponentStore struct {
 	db *sql.DB
 }
@@ -55,32 +61,37 @@ func (s *ComponentStore) GetPaginated(ctx context.Context, offset int64, search 
 	var rowCount int64
 	err := countRow.Scan(&rowCount)
 	if err != nil {
+		fmt.Println(err)
 		return nil, err
 	}
 
 	list.ResultCount = rowCount
 
-	if offset > list.GetMaxPages() {
-		return nil, errors.New("page out of bounds")
+	if offset >= list.GetMaxPages() {
+		return nil, ErrPageOutOfBounds
 	}
 
 	rowOffset := offset * internal.PAGINATION_COUNT
 	var rows *sql.Rows
 	if searchIncluded {
 		rows, err = s.db.QueryContext(ctx,
-			`SELECT COUNT(*) FROM components
+			`SELECT id, added_by, name, description, footprint, manufacturer, supplier, amount, created_at, deleted_at
+			FROM components
 			WHERE id::text LIKE '%' || $1 || '%'
-			OR name LIKE '%' || $1 || '%'
-			OR description LIKE '%' || $1 || '%'
-			OR footprint LIKE '%' || $1 || '%'
-			OR manufacturer LIKE '%' || $1 || '%'
-			OR supplier LIKE '%' || $1 || '%' LIMIT $2 OFFSET $3`, search, internal.PAGINATION_COUNT, rowOffset)
+				OR name LIKE '%' || $1 || '%'
+				OR description LIKE '%' || $1 || '%'
+				OR footprint LIKE '%' || $1 || '%'
+				OR manufacturer LIKE '%' || $1 || '%'
+				OR supplier LIKE '%' || $1 || '%'
+			LIMIT $2 OFFSET $3;`,
+			search, internal.PAGINATION_COUNT, rowOffset)
 	} else {
 		rows, err = s.db.QueryContext(ctx, `SELECT * FROM components LIMIT $1 OFFSET $2;`, internal.PAGINATION_COUNT, rowOffset)
 	}
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
 	for rows.Next() {
 		var comp model.Component
